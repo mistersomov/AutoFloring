384a41b4f02e7ff1016180e729ac6a2d87678c26
Ivan Somov <msijoc@gmail.com>
2024-11-28 23:25:08 +0300

    Fix code style

--- main/include/Device.hpp
+++ main/include/Device.hpp
@@ -6,24 +6,23 @@
 #include <memory>
 
 namespace autflr {
-	class Device {
-	public:
-		Device(i2c_master_dev_t* pHandler) : mHandler{std::move(pHandler)} {}
-		virtual ~Device() = default;
-
-	protected:
-		virtual void initialize() const = 0;
+    class Device {
+    public:
+        Device(i2c_master_dev_t* pHandler) : mHandler{std::move(pHandler)} {}
+        virtual ~Device() = default;
 
     protected:
-		struct DeviceHandlerDeleter {
-			void operator()(i2c_master_dev_t* pHandler) {
-				if (pHandler) {
-					ESP_ERROR_CHECK(i2c_master_bus_rm_device(pHandler));
-				}
-			}
-		};
-		std::unique_ptr<i2c_master_dev_t, DeviceHandlerDeleter> mHandler{nullptr};
-	};
+        virtual void initialize() const = 0;
+
+        struct DeviceHandlerDeleter {
+            void operator()(i2c_master_dev_t* pHandler) {
+                if (pHandler) {
+                    ESP_ERROR_CHECK(i2c_master_bus_rm_device(pHandler));
+                }
+            }
+        };
+        std::unique_ptr<i2c_master_dev_t, DeviceHandlerDeleter> mHandler{nullptr};
+    };
 }
 
 #endif
