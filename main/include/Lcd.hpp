22da8e1846800c228b1a2c7241b1ad4d8f3735a1
Ivan Somov <msijoc@gmail.com>
2024-11-29 20:35:18 +0300

    Refactored Lcd

--- main/include/Lcd.hpp
+++ main/include/Lcd.hpp
@@ -3,21 +3,33 @@
 
 #include "I2cSlaveDevice.hpp"
 
+#include "esp_log.h"
+
+#include <chrono>
 #include <string>
+#include <thread>
+#include <vector>
 
 namespace autflr {
-    class Lcd : I2cSlaveDevice {
+    class Lcd : public I2cSlaveDevice {
     public:
         Lcd(i2c_master_dev_t* pHandler);
 
-        void setBacklight(bool enable) const;
         void putCursor(uint8_t row, uint32_t col) const;
         void print(
             const std::string& message,
             uint8_t row,
             uint32_t col
         ) const;
-        void clear() const;
+
+        inline void clear() const {
+            sendCmd(0x01);
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        }
+
+        inline void setBacklight(bool enable) const {
+
+        }
 
     protected:
         void initialize() const override;
@@ -26,7 +38,13 @@ namespace autflr {
         void sendCmd(char cmd) const;
         void sendData(char data) const;
 
-        constexpr static const std::string TAG{"[LCD]"};
+        static constexpr uint8_t ENABLE_BIT = 0x0C;
+        static constexpr uint8_t DISABLE_BIT = 0x08;
+        static constexpr uint8_t ENABLE_DATA = 0x0D;
+        static constexpr uint8_t DISABLE_DATA = 0x09;
+        static constexpr uint8_t ROW_0_OFFSET = 0x80;
+        static constexpr uint8_t ROW_1_OFFSET = 0xC0;
+        constexpr static const char* TAG{"[LCD]"};
     };
 }
 
--- main/src/Lcd.cpp
+++ main/src/Lcd.cpp
@@ -1,11 +1,5 @@
 #include "Lcd.hpp"
 
-#include "esp_log.h"
-
-#include <thread>
-#include <chrono>
-#include <vector>
-
 namespace autflr {
     Lcd::Lcd(i2c_master_dev_t* pHandler) : I2cSlaveDevice(pHandler) {
         initialize();
@@ -36,20 +30,16 @@ namespace autflr {
         sendCmd(0x02);
         std::this_thread::sleep_for(std::chrono::milliseconds(4));
 
-        ESP_LOGI(TAG.c_str(), "Initialization is completed!");
-    }
-
-    void Lcd::setBacklight(bool enable) const {
-        sendCmd(0x00);
+        ESP_LOGI(TAG, "Initialization is completed!");
     }
 
     void Lcd::putCursor(uint8_t row, uint32_t col) const {
         switch(row) {
             case 0:
-                col |= 0x80;
+                col |= ROW_0_OFFSET;
                 break;
             case 1:
-                col |= 0xC0;
+                col |= ROW_1_OFFSET;
                 break;
         }
         sendCmd(col);
@@ -62,29 +52,24 @@ namespace autflr {
         uint32_t col
     ) const {
         putCursor(row, col);
-        for (auto i = 0; i != message.length(); ++i) {
-            sendData(message[i]);
+        for (auto itt = message.begin(); itt != message.end(); ++itt) {
+            sendData(*itt);
         }
-    }
-
-    void Lcd::clear() const {
-        sendCmd(0x01);
-        std::this_thread::sleep_for(std::chrono::milliseconds(5));
     }
 
     void Lcd::sendCmd(char cmd) const {
         char highOrderBit = cmd & 0xF0;
         char lowOrderBit = cmd << 4;
         std::vector<uint8_t> bits = {
-            static_cast<uint8_t>(highOrderBit | 0x0C),
-            static_cast<uint8_t>(highOrderBit | 0x08),
-            static_cast<uint8_t>(lowOrderBit | 0x0C),
-            static_cast<uint8_t>(lowOrderBit | 0x08)
+            static_cast<uint8_t>(highOrderBit | ENABLE_BIT),
+            static_cast<uint8_t>(highOrderBit | DISABLE_BIT),
+            static_cast<uint8_t>(lowOrderBit | ENABLE_BIT),
+            static_cast<uint8_t>(lowOrderBit | DISABLE_BIT)
         };
 
         esp_err_t err = i2c_master_transmit(mHandler.get(), bits.data(), sizeof(uint8_t) * bits.size(), 1000);
         if (err != ESP_OK) {
-            ESP_LOGE(TAG.c_str(), "Failed to send command: %d", err);
+            ESP_LOGE(TAG, "Failed to send command: %d", err);
         }
     }
 
@@ -92,15 +77,16 @@ namespace autflr {
         char highOrderBit = data & 0xF0;
         char lowOrderBit = data << 4;
         std::vector<uint8_t> bits = {
-            static_cast<uint8_t>(highOrderBit | 0x0D),
-            static_cast<uint8_t>(highOrderBit | 0x09),
-            static_cast<uint8_t>(lowOrderBit | 0x0D),
-            static_cast<uint8_t>(lowOrderBit | 0x09)
+            static_cast<uint8_t>(highOrderBit | ENABLE_DATA),
+            static_cast<uint8_t>(highOrderBit | DISABLE_DATA),
+            static_cast<uint8_t>(lowOrderBit | ENABLE_DATA),
+            static_cast<uint8_t>(lowOrderBit | DISABLE_DATA)
         };
 
         esp_err_t err = i2c_master_transmit(mHandler.get(), bits.data(), sizeof(uint8_t) * bits.size(), 1000);
         if (err != ESP_OK) {
-            ESP_LOGE(TAG.c_str(), "Failed to send data: %d", err);
+            ESP_LOGE(TAG, "Failed to send data: %d", err);
         }
     }
+
 }
